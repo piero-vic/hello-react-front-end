54c1bfdf6de24fdab9a6a495d406cab77b420b9e
Piero Lescano <pierolescano@protonmail.com>
2022-06-01 10:45:45 -0500

    feat: Display error messages when the backend is not running

--- src/redux/greeting.js
+++ src/redux/greeting.js
@@ -3,10 +3,14 @@ const UPDATE_GREETING = 'UPDATE_GREETING';
 const API_URL = 'http://localhost:9000';
 
 export const fetchGreeting = () => async (dispatch) => {
-  const response = await fetch(`${API_URL}/v1/random`);
-  const greeting = await response.json();
-  if (response.ok) {
-    dispatch({ type: UPDATE_GREETING, payload: greeting });
+  try {
+    const response = await fetch(`${API_URL}/v1/random`);
+    const greeting = await response.json();
+    if (response.ok) {
+      dispatch({ type: UPDATE_GREETING, payload: greeting });
+    }
+  } catch (error) {
+    dispatch({ type: UPDATE_GREETING, payload: { text: 'Error: The back-end should be running in port 9000' } });
   }
 };
 
